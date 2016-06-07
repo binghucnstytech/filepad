2cf4ea369245e90bde631840d0845fe981aa5481
binghucnsty <binghucnsty@gmail.com>
2016-06-08 00:07:15 +0400

    added webpack

---
+++ dev-server.js
@@ -0,0 +1,22 @@
+var WebpackDevServer = require("webpack-dev-server");
+var webpack = require("webpack");
+var compiler = require('./webpack.config');
+
+var server = new WebpackDevServer(compiler, {
+  conntentBase: "http://localhost/",
+  hot: true,
+  historyApiFallback: false,
+  compress: true,
+  quiet: false,
+  noInfo: false,
+  filename: 'bundle.js',
+  watchOptions: {
+    aggregateTimeout: 300,
+    poll: 1000
+  },
+  publicPath: "/assets/",
+});
+
+server.lister(3000, "localhost", function() {
+  console.log
+});
---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+var path = require('path');
+var webpack = require('webpack');
+
+var rootDir = path.join(__dirname, 'app');
+
+module.exports = {
+  context: path.join(rootDir, 'scripts'),
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'react-hot-loader/patch',
+    './app.js',
+  ],
+  output: {
+    path: path.join(rootDir, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+    root: [
+      path.join(rootDir, 'src'),
+    ],
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new webpack.DefinePlugin({
+      __DEV__: true,
+    }),
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.json$/,
+        loader: 'json',
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+      },
+      {
+        test: /\.scss$/,
+        loaders: [
+          'style-loader',
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ]
+      },
+      {
+        test: /\.css$/,
+        loaders: [
+          'style-loader',
+          'css-loader',
+          'postcss-loader',
+        ]
+      },
+      {
+        test: /\.(png|jpg)$/,
+        loader: 'file-loader',
+      }
+    ],
+  }
+};
