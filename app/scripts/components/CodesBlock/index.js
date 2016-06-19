3f3e6db5236649aed3da7f8ffddc667cbe141b3b
binghucnsty <binghucnsty@gmail.com>
2016-06-20 00:55:22 +0400

    added a few languages

--- app/scripts/components/CodesBlock/index.js
+++ app/scripts/components/CodesBlock/index.js
@@ -5,16 +5,95 @@ import update from 'react/lib/update';
 
 import 'codemirror/lib/codemirror.css';
 import './styles.css';
+
+import 'codemirror/mode/apl/apl';
+import 'codemirror/mode/asciiarmor/asciiarmor';
+import 'codemirror/mode/asterisk/asterisk';
+import 'codemirror/mode/clojure/clojure';
+import 'codemirror/mode/cmake/cmake';
+import 'codemirror/mode/coffeescript/coffeescript';
+import 'codemirror/mode/commonlisp/commonlisp';
+import 'codemirror/mode/crystal/crystal';
+import 'codemirror/mode/css/css';
+import 'codemirror/mode/d/d';
+import 'codemirror/mode/dart/dart';
+import 'codemirror/mode/dockerfile/dockerfile';
+import 'codemirror/mode/elm/elm';
+import 'codemirror/mode/erlang/erlang';
+import 'codemirror/mode/go/go';
+import 'codemirror/mode/groovy/groovy';
+import 'codemirror/mode/haml/haml';
+import 'codemirror/mode/haskell/haskell';
+import 'codemirror/mode/jade/jade';
 import 'codemirror/mode/javascript/javascript';
-import 'codemirror/mode/xml/xml';
+import 'codemirror/mode/jinja2/jinja2';
+import 'codemirror/mode/jsx/jsx';
+import 'codemirror/mode/lua/lua';
 import 'codemirror/mode/markdown/markdown';
+import 'codemirror/mode/nginx/nginx';
+import 'codemirror/mode/pascal/pascal';
+import 'codemirror/mode/perl/perl';
+import 'codemirror/mode/php/php';
+import 'codemirror/mode/powershell/powershell';
+import 'codemirror/mode/ruby/ruby';
+import 'codemirror/mode/rust/rust';
+import 'codemirror/mode/sass/sass';
+import 'codemirror/mode/scheme/scheme';
+import 'codemirror/mode/shell/shell';
+import 'codemirror/mode/slim/slim';
+import 'codemirror/mode/sql/sql';
+import 'codemirror/mode/stylus/stylus';
+import 'codemirror/mode/swift/swift';
+import 'codemirror/mode/tcl/tcl';
+import 'codemirror/mode/twig/twig';
+import 'codemirror/mode/xml/xml';
+import 'codemirror/mode/yaml/yaml';
 
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 const languages = [
+  'apl',
+  'asciiarmor',
+  'asterisk',
+  'clojure',
+  'cmake',
+  'coffeescript',
+  'commonlisp',
+  'crystal',
+  'css',
+  'd',
+  'dart',
+  'dockerfile',
+  'elm',
+  'erlang',
+  'go',
+  'groovy',
+  'haml',
+  'haskell',
+  'jade',
   'javascript',
-  'xml',
+  'jinja2',
+  'jsx',
+  'lua',
   'markdown',
+  'nginx',
+  'pascal',
+  'perl',
+  'php',
+  'powershell',
+  'ruby',
+  'rust',
+  'sass',
+  'scheme',
+  'shell',
+  'slim',
+  'sql',
+  'stylus',
+  'swift',
+  'tcl',
+  'twig',
+  'xml',
+  'yaml',
 ];
 
 export default class CodesBlock extends BaseBlock {
